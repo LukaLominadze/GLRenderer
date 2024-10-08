0cd255395919f7eb9fce08e4e5f5954dab05722c
MustachoMan <fguru974@gmail.com>
2024-10-08 22:32:53 +0400

    Seperating debug and release definitions

--- GLRenderer/src/DebugNMacros.h
+++ GLRenderer/src/DebugNMacros.h
@@ -5,32 +5,41 @@
 
 #include <iostream>
 
-#define DEBUG_LOG(message) \
-	std::cout << message << std::endl
-#define ERROR_LOG(message) \
-	std::cout << message << std::endl; \
-	__debugbreak()
-#define LOG(message) std::cout << message << std::endl
-#define ASSERT(x, message) \
-			std::cerr << message; \
-			if ((x)){ \
-				std::cerr << " -> Success!" << std::endl; \
-			} \
-			else { \
-				std::cerr << " -> Failed!" << std::endl; \
-				__debugbreak(); \
-			}
-#define ASSERT_ERROR(x) \
-			if (!(x)){ \
-				std::cerr << "Failed!" << std::endl; \
-				__debugbreak(); \
-			}
-#define GLCall(x) x; ASSERT_ERROR(GLLogCall(#x, __FILE__, __LINE__))
+#ifdef GL_PLATFORM_WINDOWS
+	#define ASSERT_ERROR(x) \
+				if (!(x)){ \
+					std::cerr << "Failed!" << std::endl; \
+					__debugbreak(); \
+				}
+	#define LOG(message) std::cout << message << std::endl
+	#define ERROR_LOG(message) \
+		std::cout << message << std::endl; \
+		__debugbreak()
+	#ifdef _DEBUG
+		#define GLCall(x) x; ASSERT_ERROR(GLLogCall(#x, __FILE__, __LINE__))
+		#define DEBUG_LOG(message) \
+			std::cout << message << std::endl
+		#define ASSERT(x, message) \
+					std::cerr << message; \
+					if ((x)){ \
+						std::cerr << " -> Success!" << std::endl; \
+					} \
+					else { \
+						std::cerr << " -> Failed!" << std::endl; \
+						__debugbreak(); \
+					}
+	#else
+		#define DEBUG_LOG(message)
+		#define ASSERT(x, message)
+		#define GLCall(x) x;
+	#endif
+#endif
 
 inline void GLClearError() {
     while (glGetError() != GL_NO_ERROR);
 }
 
+#ifdef _DEBUG
 inline bool GLLogCall(const char* function, const char* file, int line) {
     while (GLenum error = glGetError()) {
         std::cout << "{OpenGL Error} (" << error << "): " << function << " " << file << std::endl;
@@ -38,3 +47,4 @@ inline bool GLLogCall(const char* function, const char* file, int line) {
     }
     return true;
 }
+#endif
